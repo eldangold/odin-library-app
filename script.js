d9db79bc7443727be89db47c6f6ce111eb9f2885
Eldan Gold <56773148+eldangold@users.noreply.github.com>
2025-03-26 18:31:42 +0100

    Develop basic mechanism of displaying book information and add basic styling

--- script.js
+++ script.js
@@ -1,21 +1,55 @@
 const myLibrary = [];
 
-function Book(title, author, pages, isRead) {
+function Book(title, author, pages, isRead, id) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.isRead = isRead;
+  this.id = id;
 }
 
 function addNewBook(title, author, pages, isRead) {
-  myLibrary.push(new Book(title, author, pages, isRead));
+  myLibrary.push(new Book(title, author, pages, isRead, crypto.randomUUID()));
 }
 
 addNewBook("Dummy Book", "Dummy Author", 295, "No");
 addNewBook("Dummy Book 2", "Dummy Author 2", 255, "Yes");
+addNewBook("Dummy Book 3", "Dummy Author 3", 295, "No");
+addNewBook("Dummy Book 3", "Dummy Author 4", 255, "Yes");
 
 console.log(
   myLibrary.forEach((book) => {
     console.log(book);
   })
 );
+
+let bookshelf = document.getElementById("main-container");
+
+myLibrary.forEach((book, index) => {
+  let bookContainer = document.createElement("div");
+  bookContainer.classList.add("book-container");
+
+  let bookData = document.createElement("p");
+
+  bookData.innerHTML =
+    "<p>" +
+    "Author: " +
+    book.author +
+    "</p>" +
+    "<p>" +
+    "Pages: " +
+    book.pages +
+    "</p>" +
+    "<p>" +
+    "Read: " +
+    book.isRead +
+    "</p>";
+  bookContainer.innerHTML =
+    "<p class = 'book-title'>" +
+    book.title +
+    "</p>" +
+    "<div class = 'book-data'>" +
+    bookData.innerHTML +
+    "</div>";
+  bookshelf.appendChild(bookContainer);
+});
